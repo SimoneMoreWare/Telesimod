55643c1949ebba50cb0fafa3e37ba12b76f5dd28
Simone Candido <59175940+SimoneMoreWare@users.noreply.github.com>
2022-07-23 15:10:34 +0200

    Add files via upload

---
+++ js/sendvalue.js
@@ -0,0 +1,42 @@
+function refpage() {
+   
+    var valore = document.getElementById("refpage").value;
+
+    switch (valore){
+        case '100':
+        window.location.href = "chisono.html";
+        break;
+        case '101':
+        window.location.href = "https://www.instagram.com/simocandido/";
+        break;
+        case '102':
+        window.location.href = "https://www.facebook.com/simone.candido.370";
+        break;
+        case '103':
+        window.location.href = "https://www.linkedin.com/in/simone-candido-53b696146/";
+        break;
+        case '104':
+        window.location.href = "https://twitter.com/SimoneCandido02";
+        break;
+        case '105':
+        window.location.href = "https://www.moreware.org/wp";
+        break;
+        case '106':
+        window.location.href = "https://github.com/SimoneMoreWare";
+        break;
+        case '107':
+        window.location.href = "https://www.youtube.com/channel/UCFFSDYPL0-Fx6OvJD28GP8A";
+        break;
+        case '108':
+        window.location.href = "contatti.html";
+        break;
+        case '109':
+        window.location.href = "https://www.moreware.org/books/PC_ita.epub";
+        break;
+        case '110':
+        window.location.href = "https://drive.google.com/file/d/1ANVWqTSd2ifxHXZ7V_Z5zY4We8mdBveH/view?usp=sharing";
+        break;
+            }
+
+
+                                    }
